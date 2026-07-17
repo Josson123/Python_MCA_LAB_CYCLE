cddd63b8ec78404a2b89eb5764a35487dfb58947
Josson123 <115704001+Josson123@users.noreply.github.com>
2026-07-17 12:23:35 +0530

    Add files via upload

---
+++ Python_MCA_LAB/pd/2d_list_to_dataframe.py
@@ -0,0 +1,4 @@
+import pandas as pd
+l=[[2,'Vishal',22],[1,'Kushal',25],[1,'Aman',24]]
+df=pd.DataFrame(l,columns=['id','Name','Age'])
+print(df)
---
+++ Python_MCA_LAB/pd/Dictionary_to.py
@@ -0,0 +1,4 @@
+import pandas as pd
+d={'name':['Vinay','Kushal','Aman'],'age':[22,25,24],'occ':['Engineer','Doctor','Accountant']}
+df=pd.DataFrame(d)
+print(df)
